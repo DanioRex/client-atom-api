a812acfbe7ae949333c6c54659de6943307566dd
daniorex <danielmazur13@gmail.com>
2021-09-25 00:18:51 +0200

    Many order actions added (SetOrders not included)

--- src/Orders.php
+++ src/Orders.php
@@ -53,6 +53,29 @@ class Orders extends OrdersFactory
                 'paymentPrice' => (float)$order->paymentPrice->__toString(),
                 'paymentTaxValue' => (int)$order->paymentTaxValue->__toString(),
                 'paymentCOD' => (bool)$order->paymentCOD->__toString(),
+                'paymentBills' => array_map(function ($bill) {
+                    return [
+                        'price' => $bill->price->__toString(),
+                        'taxValue' => $bill->taxValue->__toString(),
+                    ];
+                }, $order->paymentBills->xpath('paymentBill') ?? []),
+                'shippingBills' => array_map(function ($bill) {
+                    return [
+                        'price' => $bill->price->__toString(),
+                        'taxValue' => $bill->taxValue->__toString(),
+                    ];
+                }, $order->shippingBills->xpath('shippingBill') ?? []),
+                'payments' => array_map(function ($payment) {
+                    return [
+                        'moduleKey' => $payment->moduleKey->__toString(),
+                        'voucher' => $payment->voucher->__toString(),
+                        'paymentMethodId' => $payment->paymentMethodId->__toString(),
+                        'paymentValue' => $payment->paymentValue->__toString(),
+                        'paymentTransactionId' => $payment->paymentTransactionId->__toString(),
+                        'paymentTransactionDate' => $payment->paymentTransactionDate->__toString(),
+                        'paymentCommission' => $payment->paymentCommission->__toString()
+                    ];
+                }, $order->payments->xpath('payment') ?? []),
                 'shippingMethodId' => (int)$order->shippingMethodId->__toString(),
                 'shippingMethodOptionKey' => $order->shippingMethodOptionKey->__toString(),
                 'shippingMethod' => $order->shippingMethod->__toString(),
@@ -137,7 +160,65 @@ class Orders extends OrdersFactory
                 'admin' => [
                     'email' => $order->admin->email->__toString()
                 ],
-                'products' => array_map() // TODO: Dodać mapowanie produktów do pobierania zamówień
+                'products' => array_map(function ($product) {
+                    return [
+                        'positionID' => $product->positionID->__toString(),
+                        'code' => $product->code->__toString(),
+                        'productID' => $product->productID->__toString(),
+                        'productName' => $product->productName->__toString(),
+                        'price' => $product->price->__toString(),
+                        'priceBrutto' => $product->priceBrutto->__toString(),
+                        'defaultPrice' => $product->defaultPrice->__toString(),
+                        'tax' => $product->tax->__toString(),
+                        'quantity' => $product->quantity->__toString(),
+                        'noReturn' => $product->noReturn->__toString(),
+                        'unit' => $product->unit->__toString(),
+                        'combinationID' => $product->combinationID->__toString(),
+                        'combinationName' => $product->combinationName->__toString(),
+                        'comments' => $product->comments->__toString(),
+                        'serial' => $product->serial->__toString(),
+                        'producer' => $product->producer->__toString(),
+                        'kitID' => $product->kitID->__toString(),
+                        'gratis' => $product->gratis->__toString(),
+                        'availabilityStatusId' => $product->availabilityStatusId->__toString(),
+                        'externalId' => $product->externalId->__toString(),
+                        'extras' => array_map(function ($extras) {
+                            return [
+                                'label' => $extras->attributes()['lable'] ?? null,
+                                'value' => $extras->__toString()
+                            ];
+                        }, $product->extras->xpath('extra') ?? [])
+                    ];
+                }, $order->xpath('products') ?? []),
+                'otherIdentifiers' => array_map(function ($otherIdentifier) {
+                    return [
+                        'module' => $otherIdentifier->module->__toString(),
+                        'identifier' => $otherIdentifier->identifier->__toString(),
+                        'name' => $otherIdentifier->name->__toString()
+                    ];
+                }, $order->otherIdentifiers->xpath('otherIdentifier') ?? []),
+                'order_fields' => array_map(function ($order_field) {
+                    return [
+                        'id' => $order_field->id->__toString(),
+                        'key' => $order_field->key->__toString(),
+                        'name' => $order_field->name->__toString(),
+                        'value' => $order_field->value->__toString()
+                    ];
+                }, $order->order_fields->xpath('order_field')),
+                'partner' => [
+                    'partnerID' => $order->partner->partnerID->__toString(),
+                    'email' => $order->partner->email->__toString(),
+                    'company' => $order->partner->company->__toString(),
+                    'firstname' => $order->partner->firstname->__toString(),
+                    'lastname' => $order->partner->lastname->__toString(),
+                ],
+                'salesrep' => [
+                    'email' => $order->salesrep->email->__toString(),
+                    'code' => $order->salesrep->code->__toString(),
+                    'firstname' => $order->salesrep->firstname->__toString(),
+                    'lastname' => $order->salesrep->lastname->__toString(),
+                ],
+                'offerName' => $order->offerName->__toString()
             ];
         }
         return $processed;
@@ -148,53 +229,147 @@ class Orders extends OrdersFactory
      */
     public function GetOrdersStatuses(): array
     {
-        // TODO: Implement GetOrdersStatuses() method.
+        $processed = [];
+        $xml = $this->convertToXmlElement(
+            $this->try(__FUNCTION__, [])
+        );
+        foreach ($xml->xpath('orderStatus') as $orderStatus) {
+            $processed[] = [
+                'id' => $orderStatus->id->__toString(),
+                'name' => $orderStatus->name->__toString(),
+            ];
+        }
+        return $processed;
     }
 
     /**
      * @inheritDoc
      */
-    public function GetOrderStatus(int $orderId): int
+    public function GetOrderStatus(
+        int $orderId
+    ): int
     {
-        // TODO: Implement GetOrderStatus() method.
+        return $this->try(__FUNCTION__, [
+            $orderId
+        ]);
     }
 
     /**
      * @inheritDoc
      */
-    public function GetOrderPayments(array $orders_ids): array
+    public function GetOrderPayments(
+        array $orders_ids
+    ): array
     {
-        // TODO: Implement GetOrderPayments() method.
+        $to_atomstore = [];
+        foreach ($orders_ids as $orders_id) {
+            $to_atomstore[] = [
+                'id' => $orders_id
+            ];
+        }
+        $xml = $this->convertToXmlElement(
+            $this->try(__FUNCTION__, [
+                [
+                    'xml' => $this->convertToXml($to_atomstore, 'order', 'orders')
+                ]
+            ])
+        );
+        $processed = [];
+        foreach ($xml->xpath('order') as $order) {
+            $processed[] = [
+                'id' => $order->id->__toString(),
+                'paid' => $order->paid->__toString(),
+                'payments' => array_map(function ($payment) {
+                    return [
+                        'voucher' => $payment->voucher->__toString(),
+                        'paymentValue' => $payment->paymentValue->__toString(),
+                        'paymentTransactionId' => $payment->paymentTransactionId->__toString(),
+                        'paymentTransactionDate' => $payment->paymentTransactionDate->__toString(),
+                        'paymentCommision' => $payment->paymentCommision->__toString(),
+                    ];
+                }, $order->payments->xpath('payment') ?? [])
+            ];
+        }
+        return $processed;
     }
 
     /**
      * @inheritDoc
      */
-    public function CancelReceipts(array $data): string
+    public function CancelReceipts(
+        array $data
+    ): string
     {
-        // TODO: Implement CancelReceipts() method.
+        $to_atomstore = [];
+        foreach ($data as $id) {
+            $to_atomstore[] = [
+                'id' => $id
+            ];
+        }
+        return $this->try(__FUNCTION__, [
+            [
+                'xml' => $this->convertToXml($to_atomstore, 'order', 'orders')
+            ]
+        ]);
     }
 
     /**
      * @inheritDoc
      */
-    public function SetInvoices(array $data): string
+    public function SetInvoices(
+        array $data
+    ): string
     {
-        // TODO: Implement SetInvoices() method.
+        $processed = [];
+        if (!empty($data)) {
+            foreach ($data as $element) {
+                $to_array = [];
+                if (isset($element['number'])) $to_array['number'] = (string)$element['number'];
+                if (isset($element['proforma'])) $to_array['proforma'] = (string)$element['proforma'];
+                if (isset($element['order_id'])) $to_array['order_id'] = (string)$element['order_id'];
+                if (isset($element['date_created'])) $to_array['date_created'] = (string)$element['date_created'];
+                if (isset($element['date_sale'])) $to_array['date_sale'] = (string)$element['date_sale'];
+                if (isset($element['date_payment'])) $to_array['date_payment'] = (string)$element['date_payment'];
+                if (isset($element['payment_method'])) $to_array['payment_method'] = (string)$element['payment_method'];
+                if (isset($element['payment_method_id'])) $to_array['payment_method_id'] = (string)$element['payment_method_id'];
+                if (isset($element['value'])) $to_array['value'] = (string)$element['value'];
+                if (isset($element['currency'])) $to_array['currency'] = (string)$element['currency'];
+                if (isset($element['pdf_base64'])) $to_array['pdf_base64'] = (string)$element['pdf_base64'];
+                if (isset($element['data'])) $to_array['data']['_cdata'] = (string)$element['data'];
+                if (isset($element['paid'])) $to_array['paid'] = (string)$element['paid'];
+                if (isset($element['parent'])) $to_array['parent'] = (string)$element['parent'];
+                if (isset($element['additional_orders'])) $to_array['additional_orders'] = (string)$element['additional_orders'];
+                array_push($processed, $to_array);
+            }
+        }
+        return $this->try(__FUNCTION__, [['xml' => $this->convertToXml($processed, 'invoice', 'invoices')]]);
     }
 
     /**
      * @inheritDoc
      */
-    public function SetOrderExternalId(int $orderId, string $externalId, string $externalName = "", string $moduleKey = ""): string
+    public function SetOrderExternalId(
+        int    $orderId,
+        string $externalId,
+        string $externalName = "",
+        string $moduleKey = ""
+    ): string
     {
-        // TODO: Implement SetOrderExternalId() method.
+        return $this->try(__FUNCTION__, [
+            $orderId,
+            $externalId,
+            $externalName,
+            $moduleKey
+        ]);
     }
 
     /**
      * @inheritDoc
      */
-    public function SetOrders(array $data, int $price_list_id = 0): array|string
+    public function SetOrders(
+        array $data,
+        int   $price_list_id = 0
+    ): array|string
     {
         // TODO: Implement SetOrders() method.
     }
@@ -202,16 +377,37 @@ class Orders extends OrdersFactory
     /**
      * @inheritDoc
      */
-    public function SetOrderStatus(int $orderId, int $statusId, string $emailNotification = "auto", bool $refreshModified = false): string
+    public function SetOrderStatus(
+        int    $orderId,
+        int    $statusId,
+        string $emailNotification = "auto",
+        bool   $refreshModified = false
+    ): string
     {
-        // TODO: Implement SetOrderStatus() method.
+        return $this->try(__FUNCTION__, [
+            $orderId,
+            $statusId,
+            $emailNotification,
+            $refreshModified
+        ]);
     }
 
     /**
      * @inheritDoc
      */
-    public function SetReceiptNumbers(array $data): string
+    public function SetReceiptNumbers(
+        array $data
+    ): string
     {
-        // TODO: Implement SetReceiptNumbers() method.
+        $processed = [];
+        if (!empty($data)) {
+            foreach ($data as $element) {
+                $to_array = [];
+                if (isset($element['id'])) $to_array['id'] = (string)$element['id'];
+                if (isset($element['paragon'])) $to_array['paragon'] = (string)$element['paragon'];
+                array_push($processed, $to_array);
+            }
+        }
+        return $this->try(__FUNCTION__, [['xml' => $this->convertToXml($processed, 'order', 'orders')]]);
     }
 }
